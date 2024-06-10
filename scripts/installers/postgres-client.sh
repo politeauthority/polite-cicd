426f5ecb874df45a197d84ff4ca27fdf40158cd4
Alix Fullerton <fullertonalix0@gmail.com>
2024-06-10 15:57:11 -0600

    Last attempt for now

--- scripts/installers/postgres-client.sh
+++ scripts/installers/postgres-client.sh
@@ -3,7 +3,7 @@
 set -e
 echo "Installing Postgres Client"
 # Import the repository signing key:
-apt install curl ca-certificates
+apt-get install curl ca-certificates
 install -d /usr/share/postgresql-common/pgdg
 curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
@@ -11,9 +11,9 @@ curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://
 sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Update the package lists:
-apt update
+apt-get update
 
 # Install the latest version of PostgreSQL:
 # If you want a specific version, use 'postgresql-16' or similar instead of 'postgresql'
-apt -y install postgresql-client-16
+apt-get -y install postgresql-client-16
 echo "Successfully installed Postgres Client"
